0c4d5f79d232f6877008b4da40cc59294504754a
somnath-rava-ai <somnath@rava.ai>
2025-04-23 21:35:55 +0530

    fix: added logging and /health

--- app.py
+++ app.py
@@ -10,8 +10,13 @@ from fastapi import FastAPI, HTTPException
 from crawl4ai import AsyncWebCrawler, BrowserConfig, CrawlerRunConfig
 from crawl4ai.async_dispatcher import MemoryAdaptiveDispatcher
 from crawl4ai import CrawlerMonitor, DisplayMode, RateLimiter
-
-
+import traceback
+import logging
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s %(name)s %(message)s",
+    level=logging.INFO,
+)
+logger = logging.getLogger("my_fastapi_app")
 crawler = None
 monitor = None
 @asynccontextmanager
@@ -25,21 +30,27 @@ async def lifespan(app: FastAPI):
     await crawler.start()
     monitor = CrawlerMonitor(
     )
-    
+    logger.info("initialized crawler monitor")
     yield  # This allows the app to run
 
 
 app = FastAPI(lifespan=lifespan)
 
 
+@app.get("/health")
+async def health(payload: dict):
+    return {"status": 200, "message": "online"}
+
 @app.post("/crawl")
 async def crawl_endpoint(payload: dict):
     urls = payload.get("urls")
     try:
+        logger.info("Running /crawler endpoint")
         results = await run_crawler(urls)
         # print(results[0])
         return {"status": 200, "number_of_pages":len(results),  "results": results}
-    except:
+    except Exception as e:
+        print(traceback.format_exc())
         return {"status": 400, "message": "unable_to_scrape"}
 
 
@@ -86,13 +97,17 @@ async def run_crawler(urls):
     for result in results:
         if result.success:
             md = result.markdown
-            html  = result.html
             clean_md = re.sub(pattern, '', md)
             texts = clean_md.strip("\n")
-            soup = BeautifulSoup(html, 'html.parser')
-            title = soup.find('title').text
+            try:
+                html  = result.html
+                soup = BeautifulSoup(html, 'html.parser')
+                title = soup.find('title').text 
+            except:
+                title = result.url.split('/')[-1]
+                logger.error(f"Could not find title for {result.url}")
             md_results.append({"url": result.url, "title": title, "page_content": texts})
         else:
             print(f"Failed to crawl {result.url}: {result.error_message}")
-    print(time.time() - start)
+    logger.info(f"Time taken to complete request with {len(md_results)}urls {time.time() - start}")
     return md_results
